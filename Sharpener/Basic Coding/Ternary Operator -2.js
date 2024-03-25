5445ada818e719ac2bdd69d00a2f3521be57adae
kesavaprasad08 <kesav2661998@gmail.com>
2024-03-25 20:20:56 +0530

    Ternary Operator -2

---
+++ Sharpener/Basic Coding/Ternary Operator -2.js
@@ -0,0 +1,12 @@
+// Write a program to print the maximum of two numbers using Ternary operator.
+// Example:-
+// Input:-
+// a=3, b=5
+// Output:-
+// 5
+
+function max(a, b) {
+  console.log(a > b ? a : b);
+}
+
+max(650, 650);
