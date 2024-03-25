e251ec00cf5c49f9271a373e77373b3dfa068283
kesavaprasad08 <kesav2661998@gmail.com>
2024-03-25 20:17:38 +0530

    Ternary Operators - 1

---
+++ Sharpener/Basic Coding/Ternary Operators - 1.js
@@ -0,0 +1,12 @@
+// Write a program to assume a value of marks and print whether a person has failed or passed using ternary Operator.
+// If Marks> =40 --> pass
+// ELSE ---> fail
+// Input:-
+// Marks =52
+// Output:-
+// pass
+
+function hasPassed(mark){
+    console.log((mark>=40)? 'PASS':'FAIL')
+}
+hasPassed(65);
