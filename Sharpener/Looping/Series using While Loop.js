6b519fc907f4a7af09e61f8f7aa8b209a1910f13
kesavaprasad08 <kesav2661998@gmail.com>
2024-03-26 18:41:20 +0530

    Series using While Loop

---
+++ Sharpener/Looping/Series using While Loop.js
@@ -0,0 +1,19 @@
+// Print the following series using while loop
+// 1 4 9 16 25 36 …. n
+// Input :-
+// n = 49
+// Output :-
+// 1
+// 4
+// 9
+// 16
+// 25
+// 36
+// 49
+
+let n=49;
+let i=1;
+while(i*i <= n){
+    console.log(i*i)
+    i++;
+}
