70b265b504b2ddb0fa7bd882a7e62efa069b1c47
kesavaprasad08 <kesav2661998@gmail.com>
2024-03-26 19:08:36 +0530

    Odd Number Series

---
+++ Sharpener/Looping/Odd Number Series.js
@@ -0,0 +1,15 @@
+// Write a program to print all odd numbers from 1 to n using for loops
+// Example:-
+// Input :-
+// n=7
+// Output :-
+// 1
+// 3
+// 5
+// 7
+
+let n=7;
+
+for(let i=1;i<=7;i=i+2){
+    console.log(i)
+}
