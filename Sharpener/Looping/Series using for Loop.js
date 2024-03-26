d7d434c78f4b58524228f59c1da1197a98262d7c
kesavaprasad08 <kesav2661998@gmail.com>
2024-03-26 19:15:03 +0530

    Series using for Loop

---
+++ Sharpener/Looping/Series using for Loop.js
@@ -0,0 +1,16 @@
+// Print the following series using for loop:-
+// 1,8,27,64,125,216,......n
+// Input
+// 125
+// Output :-
+// 1
+// 8
+// 27
+// 64
+// 125
+
+let n=220;
+
+for(let i=1;(i*i*i)<=n;i++){
+    console.log(i*i*i)
+}
