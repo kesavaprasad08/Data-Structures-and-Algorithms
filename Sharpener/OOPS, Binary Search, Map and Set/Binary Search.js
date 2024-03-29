e7fdad81faeceabfe583190c3458cf747d900eb3
kesavaprasad08 <kesav2661998@gmail.com>
2024-03-29 23:10:41 +0530

    binary search

---
+++ Sharpener/OOPS, Binary Search, Map and Set/Binary Search.js
@@ -0,0 +1,18 @@
+
+var search = function(nums, target) {
+    let start =0;
+    let end = nums.length-1;
+    while(start<=end){        
+    let mid =Math.floor((end+start)/2)
+     if(nums[mid]==target){
+        return mid
+     }else if(target>nums[mid]){
+        start=mid+1;
+
+     }else{
+        end=mid-1
+     } 
+       
+    }
+    return -1;
+};
