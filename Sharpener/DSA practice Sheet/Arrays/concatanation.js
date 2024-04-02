1d16407f612848f049a63a52ac0fb4e35ad4e9e2
kesavaprasad08 <kesav2661998@gmail.com>
2024-04-02 21:44:54 +0530

    concat array

---
+++ Sharpener/DSA practice Sheet/Arrays/concatanation.js
@@ -0,0 +1,8 @@
+var getConcatenation = function(nums) {
+    let ans=[];
+    for(let i=0;i<nums.length;i++){    
+        ans[i]=nums[i];
+        ans[i+nums.length]=nums[i]
+    }
+    return ans;
+};
