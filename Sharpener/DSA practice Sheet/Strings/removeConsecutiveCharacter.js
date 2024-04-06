407b4f6788b3bfc71aeeb10af13cf53e24b384f0
kesavaprasad08 <kesav2661998@gmail.com>
2024-04-06 20:12:55 +0530

    remove consecutive

---
+++ Sharpener/DSA practice Sheet/Strings/removeConsecutiveCharacter.js
@@ -0,0 +1,14 @@
+removeConsecutiveCharacter(s){
+    //code here
+    
+    let res='';
+    let i=0;
+    while(i<s.length){
+        
+        if(s[i]!==s[i+1]){
+            res+=s[i];
+        }
+        i++;
+    }
+return res
+}
