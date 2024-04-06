0d1fd6f5818b6e6f57281694ebc2672aa0961878
kesavaprasad08 <kesav2661998@gmail.com>
2024-04-06 22:02:58 +0530

    valid parenthesis

---
+++ Sharpener/DSA practice Sheet/Strings/Valid Parentheses.js
@@ -0,0 +1,14 @@
+var isValid = function (s) {
+    let sta = [];
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+            sta.push(s[i])
+        } else if (!sta.length || s[i] == ')' && sta[sta.length - 1] !== '(' || s[i] == '}' && sta[sta.length - 1] !== '{' || s[i] == ']' && sta[sta.length - 1] !== '[') {
+            return false;
+        }
+        else
+            sta.pop()
+    }
+
+    return sta.length == 0
+};
