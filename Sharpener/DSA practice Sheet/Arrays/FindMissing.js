ab9e103a8e34ba00a7c637386b68699b08ace79d
kesavaprasad08 <kesav2661998@gmail.com>
2024-04-07 23:55:14 +0530

    findMissing

---
+++ Sharpener/DSA practice Sheet/Arrays/FindMissing.js
@@ -0,0 +1,9 @@
+function findMissing(arr,n){
+    //code here
+    let dif = (arr[n-1]-arr[0])/n;
+    for(let i=0;i<n;i++){
+        if(arr[i+1]-arr[i]!==dif){
+            return dif+arr[i]
+        }
+    }
+ }
