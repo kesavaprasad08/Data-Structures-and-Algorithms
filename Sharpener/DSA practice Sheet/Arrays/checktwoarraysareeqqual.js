6b31bdd8801d6054784704de0cd59741ea276fbb
kesavaprasad08 <kesav2661998@gmail.com>
2024-04-07 23:49:00 +0530

    check two arrays are equal.js

---
+++ Sharpener/DSA practice Sheet/Arrays/checktwoarraysareeqqual.js
@@ -0,0 +1,11 @@
+function check(A,B,N){
+    // code here
+    A.sort((a,b)=>a-b);
+    B.sort((a,b)=>a-b);
+    for(let i=0;i<N;i++){
+        if(A[i]!==B[i]){
+            return 0;
+        }
+    }
+    return 1;
+}
