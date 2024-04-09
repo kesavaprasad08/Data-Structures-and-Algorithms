806e3065232195e09e57ca728b50286218328a21
kesavaprasad08 <kesav2661998@gmail.com>
2024-04-09 22:42:07 +0530

    getminMax

---
+++ Sharpener/DSA practice Sheet/Arrays/getMinMax.js
@@ -0,0 +1,14 @@
+getMinMax(arr,n){
+    //code here
+    let min=arr[0];
+    let max=arr[0];
+    for(let i=1;i<n;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+        if(arr[i]<min){
+            min=arr[i]
+        }
+    }
+    return [min,max]
+}
