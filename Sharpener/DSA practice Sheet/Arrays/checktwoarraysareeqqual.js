b6b871671e03b5d479c2ae09b1af617c1876dd1a
kesavaprasad08 <kesav2661998@gmail.com>
2024-04-10 23:54:42 +0530

    f

--- Sharpener/DSA practice Sheet/Arrays/checktwoarraysareeqqual.js
+++ Sharpener/DSA practice Sheet/Arrays/checktwoarraysareeqqual.js
@@ -1,10 +1,10 @@
-function check(A,B,N){
-    A.sort((a,b)=>a-b);
-    B.sort((a,b)=>a-b);
-    for(let i=0;i<N;i++){
-        if(A[i]!==B[i]){
-            return 0;
-        }
+function check(A, B, N) {
+  A.sort((a, b) => a - b);
+  B.sort((a, b) => a - b);
+  for (let i = 0; i < N; i++) {
+    if (A[i] !== B[i]) {
+      return 0;
     }
-    return 1;
+  }
+  return 1;
 }
