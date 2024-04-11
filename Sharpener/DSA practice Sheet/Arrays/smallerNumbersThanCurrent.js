1850bcbb8e3fbe6f42a28d46856f579752857534
kesavaprasad08 <kesav2661998@gmail.com>
2024-04-11 23:42:09 +0530

    c

--- Sharpener/DSA practice Sheet/Arrays/smallerNumbersThanCurrent.js
+++ Sharpener/DSA practice Sheet/Arrays/smallerNumbersThanCurrent.js
@@ -1,14 +1,14 @@
 var smallerNumbersThanCurrent = function (nums) {
-    let res=[];
-    for(let i=0;i<nums.length;i++){
-        let cur=0;
-        for(let j=0;j<nums.length;j++){
-            if(nums[i]>nums[j]){
-                cur++;
-            }
-        }
-        res.push(cur)
+  let res = [];
+  for (let i = 0; i < nums.length; i++) {
+    let cur = 0;
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[i] > nums[j]) {
+        cur++;
+      }
     }
+    res.push(cur);
+  }
 
-    return res;
+  return res;
 };
