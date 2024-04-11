3dc2349a03d39fbb791b9bdfb28ce4f5025adc67
kesavaprasad08 <kesav2661998@gmail.com>
2024-04-11 23:42:49 +0530

    d

--- Sharpener/DSA practice Sheet/Arrays/minMax.js
+++ Sharpener/DSA practice Sheet/Arrays/minMax.js
@@ -1,16 +1,16 @@
-function minMax(arr){
-    let res=[];
-    let min=arr[0];
-    let max=arr[0];
-    for(let i=1;i<arr.length;i++){
-        if(arr[i]>max){
-            max=arr[i]
-        }
-        if(arr[i]<min){
-            min=arr[i]
-        }
+function minMax(arr) {
+  let res = [];
+  let min = arr[0];
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
     }
-    return [max,min]
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  return [max, min];
 }
 
-console.log(minMax([4,6,8,7,6,3,2,4]))
+console.log(minMax([4, 6, 8, 7, 6, 3, 2, 4]));
