b4c3debd334045b49f15e4a9ac6426cef4ebd6e9
kesavaprasad08 <kesav2661998@gmail.com>
2024-04-11 23:44:09 +0530

    f

--- Sharpener/DSA practice Sheet/Arrays/kidsWithCandies.js
+++ Sharpener/DSA practice Sheet/Arrays/kidsWithCandies.js
@@ -1,8 +1,8 @@
-var kidsWithCandies = function(candies, extraCandies) {
-    let res=[];
-    let max=Math.max(...candies);
-    for(let i=0;i<candies.length;i++){
-        res[i]=candies[i]+extraCandies>=max
-    }
-    return res
+var kidsWithCandies = function (candies, extraCandies) {
+  let res = [];
+  let max = Math.max(...candies);
+  for (let i = 0; i < candies.length; i++) {
+    res[i] = candies[i] + extraCandies >= max;
+  }
+  return res;
 };
