fc6323ebc9016a33e55f8648da7e1b1215c98a5e
kesavaprasad08 <kesav2661998@gmail.com>
2024-04-11 23:40:24 +0530

    a

--- Sharpener/DSA practice Sheet/Strings/countConsistentStrings.js
+++ Sharpener/DSA practice Sheet/Strings/countConsistentStrings.js
@@ -1,18 +1,17 @@
 var countConsistentStrings = function (allowed, words) {
-    let temp = new Set(allowed);
-     let res=0;
-    words.forEach(ele => {
-     let cur=true;
-     for(const char of ele){
-         if(!temp.has(char)){
-             cur=false;
-             break;
-         }
-     }
-     if(cur){
-         res++;
-     }
- 
-    })
-   return res;
- };
+  let temp = new Set(allowed);
+  let res = 0;
+  words.forEach((ele) => {
+    let cur = true;
+    for (const char of ele) {
+      if (!temp.has(char)) {
+        cur = false;
+        break;
+      }
+    }
+    if (cur) {
+      res++;
+    }
+  });
+  return res;
+};
