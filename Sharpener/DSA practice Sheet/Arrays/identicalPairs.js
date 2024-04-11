86fbfa2005cce2cffccd0b7f00f75fa997fbe580
kesavaprasad08 <kesav2661998@gmail.com>
2024-04-11 23:44:26 +0530

    g

--- Sharpener/DSA practice Sheet/Arrays/identicalPairs.js
+++ Sharpener/DSA practice Sheet/Arrays/identicalPairs.js
@@ -1,11 +1,11 @@
 var numIdenticalPairs = function (nums) {
-    let res = 0;
-    for(let i=0;i<nums.length-1;i++){
-        for(let j=i+1;j<nums.length;j++){
-            if(nums[i]==nums[j]){
-                res++
-            }
-        }
+  let res = 0;
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] == nums[j]) {
+        res++;
+      }
     }
-    return res;
-}; 
+  }
+  return res;
+};
